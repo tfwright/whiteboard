14f4d73559ce1b922ada6b7ac037d3f725527093
tfwright <tfwright@gmail.com>
2010-08-31 17:17:26 -0500

    inception\!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whiteboard::Application.config.secret_token = '2d3d0e73c3f5d5935aac268e14eedff1eee56ee5d413ecacd81d019a8d9ab0e98eb6531a204a1d9b98d9892ef56809f315ef7c9b2981b59b0617b4128b3ed6b5'
