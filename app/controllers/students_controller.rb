80171def8270f93471e0685a2607a4f487f2b70c
tfwright <tfwright@gmail.com>
2010-09-02 00:56:02 -0500

    adds basic workflow for adding students, improves course area layout

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,22 @@ class StudentsController < ApplicationController
     @students = Course.find(params[:course_id]).students
   end
   
+  def new
+    @student = User.new
+  end
+  
+  def create
+    @student = User.new(params[:user].merge(:password => Devise.friendly_token))
+    respond_to do |format|
+      if @student.save
+        Course.find(params[:course_id]).students << @student
+        format.html { redirect_to(course_students_path(Course.find(params[:course_id])), :notice => 'Student was successfully added.') }
+        format.xml  { render :xml => @student, :status => :created, :location => @student }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @student.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
 end
