f6aec4588b1ed7a81128f53bac2f9eb6369de662
tfwright <tfwright@gmail.com>
2010-09-05 01:46:49 -0500

    fixes seed STI syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
-User.create(:name => "Admin", :email => "admin@whiteboard.tfwright.info", :password => "r1np0ch3", :type => "Admin")
+Admin.create(:name => "Admin", :email => "admin@whiteboard.tfwright.info", :password => "r1np0ch3")
