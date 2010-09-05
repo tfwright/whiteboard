26c6d338b7b913d457dfae0e3b3170c2eaa29b43
tfwright <tfwright@gmail.com>
2010-09-05 14:13:05 -0500

    adds some tests for students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,20 +8,6 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
   
-  def create
-    @student = Student.new(params[:student].merge(:password => Devise.friendly_token))
-    respond_to do |format|
-      if @student.save
-        Course.find(params[:course_id]).students << @student
-        format.html { redirect_to(course_students_path(Course.find(params[:course_id])), :notice => 'Student was successfully added.') }
-        format.js  { render :json => @student, :status => :created, :location => course_student_url(Course.find(params[:course_id]), @student) }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @student.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-  
   def enroll
     @student = Student.find_by_email(params[:student][:email]) || Student.new(params[:student].merge(:password => Devise.friendly_token))
     @student.courses << Course.find(params[:course_id])
--- test/functional/students_controller_test.rb
+++ test/functional/students_controller_test.rb
@@ -2,4 +2,29 @@ require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
 
+  test "should display new student form" do
+    sign_in Factory(:professor)
+    get :new, :course_id => Factory(:course).id
+    assert_response :success
+  end
+  
+  test "should display a roster" do
+    sign_in Factory(:professor)
+    post :import, :roster => fixture_file_upload('/files/roster.csv', 'text/csv'), :course_id => Factory(:course).id
+    assert_response :success
+  end
+  
+  test "should create student record if it doesn't exist" do
+    sign_in Factory(:professor)
+    assert_difference "Student.count", 1 do
+      post :enroll, :student => {:email => "test@example.com"}, :course_id => Factory(:course).id
+    end
+  end
+  
+  test "should list students in the course" do
+    sign_in Factory(:professor)
+    get :index, :course_id => Factory(:course).id
+    assert_response :success
+  end
+  
 end
