c80219cf9b9c8c0cb18ede748377aa9b1645a248
tfwright <tfwright@gmail.com>
2010-09-05 20:54:28 -0500

    implements user editing, fixes incorrect change-password url in notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      redirect_to @user, :notice => "Updated your account information."
+      redirect_to user_path(@user), :notice => "Updated your account information."
     else
       render :action => :edit
     end
