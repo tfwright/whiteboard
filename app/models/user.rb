99b565ddbd8bc011e489f60ee817585d81cd88bb
tfwright <tfwright@gmail.com>
2010-09-08 16:06:17 -0500

    adds email uniqueness validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   
   validates_format_of :email, :with => /^.+@.+\..+$/
+  validates_uniqueness_of :email
   
   def before_create
     self.name ||= email.gsub(/@.*/, "")
