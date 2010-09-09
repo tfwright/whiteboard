0e5053428e659129116836bbad103539dded4c4b
tfwright <tfwright@gmail.com>
2010-09-09 00:18:52 -0500

    fixes announcement bug [2]

--- test/functional/announcements_controller_test.rb
+++ test/functional/announcements_controller_test.rb
@@ -2,4 +2,12 @@ require 'test_helper'
 
 class AnnouncementsControllerTest < ActionController::TestCase
 
+  test "shows announcement" do
+    course = Factory(:course)
+    student = Factory(:student)
+    course.students << student
+    sign_in student
+    get :show, :course_id => 1, :id => Factory(:announcement).id
+  end
+  
 end
