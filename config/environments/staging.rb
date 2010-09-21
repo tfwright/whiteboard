dd86ed71a5dc5a9ad18c22bae1ab5a3c52196991
tfwright <tfwright@gmail.com>
2010-09-21 15:29:50 -0500

    attempts to set up s3 alias properly

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -57,5 +57,5 @@ PAPERCLIP_OPTIONS = {:storage => :s3,
   :s3_credentials => {:access_key_id => '0141VS3DG9BCVMMS9W82', :secret_access_key => 'i70QHTiCvBVBPoJwzs91B6zzStWQ14wsreTCmsY+'},
   :path => ":attachment/:id/:filename",
   :bucket => 'assets.tfwright-whiteboard.staging.heroku.com',
-  :s3_host_alias => 'assets.staging.whiteboard.com',
+  :s3_host_alias => 'assets.staging.whiteboard.depaulphilgrad.com',
   :url => ":s3_alias_url"}
