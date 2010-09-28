107149feb1f62597a8589012a184fac09305d676
tfwright <tfwright@gmail.com>
2010-09-28 00:05:31 -0500

    allow extra credit on grades (scores greater than 100)

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -3,6 +3,6 @@ class Grade < ActiveRecord::Base
   belongs_to :assignment
   
   validates_presence_of :student_id, :assignment_id, :score
-  validates_numericality_of :score, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100
+  validates_numericality_of :score, :greater_than_or_equal_to => 0
   validates_uniqueness_of :assignment_id, :scope => :student_id
 end
