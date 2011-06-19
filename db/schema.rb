bc23544f498d774b252bb2525112a36f748f3fb5
tfwright <tfwright@gmail.com>
2011-06-19 12:44:58 -0500

    updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(:version => 20101213025624) do
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
