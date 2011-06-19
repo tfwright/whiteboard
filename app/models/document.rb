2379784170d227d63696b6fa06c7e274f2644fcf
tfwright <tfwright@gmail.com>
2011-06-19 17:50:51 -0500

    new document form copy tweaks

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,8 @@ class Document < ActiveRecord::Base
   belongs_to :attachable, :polymorphic => true
     
   validates_attachment_presence :attached
-  validates_attachment_content_type :attached, :content_type => ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+  validates_attachment_content_type :attached, :content_type => ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
+    :message => "is not an acceptable file type."
   validates_attachment_size :attached, :less_than => 4.megabytes 
   
   validates_presence_of :name, :description, :attachable_id
