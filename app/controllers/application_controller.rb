a025a5993216cc7aa7e18f673c066435e25f5b99
tfwright <tfwright@gmail.com>
2011-06-19 18:39:56 -0500

    don't set timezone for session handling actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-  before_filter :set_timezone
+  before_filter :set_timezone, :unless => Proc.new { devise_controller? }
   
   private
   
