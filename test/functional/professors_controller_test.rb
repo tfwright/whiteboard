646ca4a3cbd6210d8ec4fbfbe99e37cc7af75267
tfwright <tfwright@gmail.com>
2011-06-19 13:15:11 -0500

    adding professors controller test

--- test/functional/professors_controller_test.rb
+++ test/functional/professors_controller_test.rb
@@ -2,10 +2,16 @@ require 'test_helper'
 
 class ProfessorsControllerTest < ActionController::TestCase
 
-  test "students redirected away" do
+  test "students redirected away from new professor form" do
     sign_in Factory(:student)
     get :new
     assert_response :redirect
+  end
+  
+  test "admins admitted to new professor form" do
+    sign_in Factory(:admin)
+    get :new
+    assert_response :success
   end
   
 end
