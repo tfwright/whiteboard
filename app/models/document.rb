2c954082357ab4927e544fde2272dd7a566ebb6c
tfwright <tfwright@gmail.com>
2012-01-21 15:16:40 -0600

    increasing file size limits for course documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,7 +5,7 @@ class Document < ActiveRecord::Base
   validates_attachment_presence :attached
   validates_attachment_content_type :attached, :content_type => ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
     :message => "is not an acceptable file type."
-  validates_attachment_size :attached, :less_than => 4.megabytes 
+  validates_attachment_size :attached, :less_than => 10.megabytes 
   
   validates_presence_of :name, :description, :attachable_id
   
