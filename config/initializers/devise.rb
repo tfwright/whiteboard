a86fabdd6eb8ade1ef117eb2a82de60326fb7caa
tfwright <tfwright@gmail.com>
2012-02-17 20:08:37 -0600

    Devise case insensitivity doesn't actually work

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -139,7 +139,6 @@ Devise.setup do |config|
   #   end
   #   manager.default_strategies(:scope => :user).unshift :twitter_oauth
   # end
-  config.case_insensitive_keys = [:email]
   config.use_salt_as_remember_token = true
   config.reset_password_within = 6.hours
 end
