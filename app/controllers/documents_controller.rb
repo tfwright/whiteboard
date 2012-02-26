9ded7fe3b5ca62d13811d100d2ab413890ac29c5
tfwright <tfwright@gmail.com>
2012-02-25 18:23:13 -0600

    Removed unused document logic/views

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,28 +3,6 @@ class DocumentsController < ApplicationController
   before_filter :set_current_course
   before_filter :ensure_professor_or_admin, :except => :show
   before_filter :ensure_enrolled
-    
-  # GET /Documents
-  # GET /Documents.xml
-  def index
-    @documents = Document.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @documents }
-    end
-  end
-
-  # GET /Documents/1
-  # GET /Documents/1.xml
-  def show
-    @document = Document.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @document }
-    end
-  end
 
   # GET /Documents/new
   # GET /Documents/new.xml
@@ -37,11 +15,6 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # GET /Documents/1/edit
-  def edit
-    @document = Document.find(params[:id])
-  end
-
   def create
     @document = Document.new(params[:document].merge(params[:attachable]))
     respond_to do |format|
@@ -52,27 +25,6 @@ class DocumentsController < ApplicationController
         format.html { render :action => "new" }
         format.xml  { render :xml => @document.errors, :status => :unprocessable_entity }
       end
-    end
-  end
-
-  def update
-    @document = Document.find(params[:id])
-    if @document.update_attributes(params[:document])
-      redirect_to(course_path(params[:course_id]), :notice => 'Document was successfully updated.')
-    else
-      render :action => "edit"
-    end
-  end
-
-  # DELETE /Documents/1
-  # DELETE /Documents/1.xml
-  def destroy
-    @document = Document.find(params[:id])
-    @document.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(documents_url) }
-      format.xml  { head :ok }
     end
   end
   
